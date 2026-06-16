529607053deec74f8f3001cad06b454cf9252e6c
yanghuachao <1659641771@qq.com>
2026-06-16 10:37:44 +0800

    Initial Project Files

---
+++ Source/LearingAura/LearingAura.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LearingAura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LearingAura, "LearingAura" );
+
+DEFINE_LOG_CATEGORY(LogLearingAura)
+ 
---
+++ Source/LearingAura/LearingAuraGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LearingAuraGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALearingAuraGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALearingAuraGameMode();
+};
+
+
+
