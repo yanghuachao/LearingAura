ea4f86151204d3d36a7f92ddd33f1d215cc68e00
yanghuachao <1659641771@qq.com>
2026-06-16 21:39:52 +0800

    Enhanced Input

---
+++ Source/LearingAura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,5 @@
+// OnlyLearn.
+
+
+#include "Player/AuraPlayerController.h"
+
---
+++ Source/LearingAura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// OnlyLearn.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARINGAURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
