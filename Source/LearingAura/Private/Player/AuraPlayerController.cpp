8fb32413c18b1a849937447660b2b42dbe3d5d0c
yanghuachao <1659641771@qq.com>
2026-06-17 00:10:36 +0800

    Movement Input

--- Source/LearingAura/Private/Player/AuraPlayerController.cpp
+++ Source/LearingAura/Private/Player/AuraPlayerController.cpp
@@ -3,3 +3,56 @@
 
 #include "Player/AuraPlayerController.h"
 
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates=true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	check(AuraContext);
+	
+	UEnhancedInputLocalPlayerSubsystem* subsystem=ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(subsystem);
+	subsystem->AddMappingContext(AuraContext,0);
+	
+	bShowMouseCursor=true;
+	DefaultMouseCursor=EMouseCursor::Default;
+	
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+	
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	UEnhancedInputComponent* EnhancedInputComponent=CastChecked<UEnhancedInputComponent>(InputComponent);
+	
+	EnhancedInputComponent->BindAction(MoveAction,ETriggerEvent::Triggered,this,&AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector=InputActionValue.Get<FVector2D>();
+	const FRotator Rotation=GetControlRotation();
+	const FRotator YawRotation(0.f,Rotation.Yaw,0.f);
+	
+	const FVector ForWardDirection=FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection=FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForWardDirection,InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection,InputAxisVector.X);
+	}
+	
+}
--- Source/LearingAura/Public/Player/AuraPlayerController.h
+++ Source/LearingAura/Public/Player/AuraPlayerController.h
@@ -6,6 +6,11 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
+
+class UInputMappingContext;
+class UInputAction;
+
+struct FInputActionValue;
 /**
  * 
  */
@@ -13,5 +18,18 @@ UCLASS()
 class LEARINGAURA_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+private:
+	AAuraPlayerController();
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
 	
+private:
+	UPROPERTY(EditAnywhere,Category="Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
+	
+	UPROPERTY(EditAnywhere,Category="Input")
+	TObjectPtr<UInputAction> MoveAction;
+	
+	void Move(const FInputActionValue& InputActionValue);
 };
